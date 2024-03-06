bcbd6fe91c87a4d06d652ce8e73bcec5557681b9
dlvlfdn567 <dlvlfdn567@gmail.com>
2024-03-06 14:23:06 +0900

    프로젝트1 실습

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,14 +1,31 @@
-const panels = document.querySelectorAll('.panel')
+let panels = document.querySelectorAll(".panel");
+console.log("panels: {}", panels);
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    const currentContainer = panel.parentElement;
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+    // 먼저, 현재 컨테이너에서 모든 패널의 활성화 상태를 제거합니다.
+    removeActiveClasses(currentContainer, "active1", "active2");
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+    const isActive =
+      panel.classList.contains("active1") ||
+      panel.classList.contains("active2");
+
+    // 클릭된 패널이 이미 활성화 상태가 아니었다면, 활성화 클래스를 추가합니다.
+    if (!isActive) {
+      if (currentContainer.classList.contains("container1")) {
+        panel.classList.add("active1");
+      } else if (currentContainer.classList.contains("container2")) {
+        panel.classList.add("active2");
+      }
+    }
+  });
+});
+
+function removeActiveClasses(container, ...activeClassesToRemove) {
+  activeClassesToRemove.forEach((activeClass) => {
+    container.querySelectorAll(`.${activeClass}`).forEach((panel) => {
+      panel.classList.remove(activeClass);
+    });
+  });
 }
